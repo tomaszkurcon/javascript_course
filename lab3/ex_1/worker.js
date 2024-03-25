5bada9139a322a43f566befa6aa00f49216d1ca1
Tomasz <tomekkurcon1@gmail.com>
2024-03-25 10:26:20 +0100

    Solution to exercise 1 lab 3, web workers

---
+++ lab3/ex_1/worker.js
@@ -0,0 +1,20 @@
+self.onmessage = (e) => {
+    const {iterations} = e.data;
+    var primes = [];
+            for (var i = 0; i < iterations; i++) {
+                var candidate = i * (1000000000 * Math.random());
+                var isPrime = true;
+                for (var c = 2; c <= Math.sqrt(candidate); ++c) {
+                    if (candidate % c === 0) {
+                        // not prime
+                        isPrime = false;
+                        break;
+                    }
+                } 
+                if (isPrime) {
+                    primes.push(candidate);
+                }
+            }
+    self.postMessage(primes);
+    close();
+}
